48cb2ab93ca2430bc14fdccc0f9edcff6f6f2268
baoxinan <960877055@qq.com>
2018-06-20 16:02:49 +0800

    baoxinan

---
+++ src/main/java/com/course/chapter/httpClient/MyCookiesForGet.java
@@ -0,0 +1,39 @@
+package com.course.chapter.httpClient;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+public class MyCookiesForGet {
+
+	private String url;
+	//ResourceBundle读取配置文件
+	private ResourceBundle bundle;
+	@BeforeTest
+	public void beforeTest(){
+		bundle = ResourceBundle.getBundle("application", Locale.CHINA);
+		url = bundle.getString("test.url");
+	}
+	@Test
+	public void TestGetCookies() throws IOException{
+		String result;
+		//从配置文件中 拼接测试的url
+		String uri = bundle.getString("getCookies.uri");
+		String testUrl = url + uri;
+		//测试逻辑代码书写
+		HttpGet get = new HttpGet(testUrl);
+		HttpClient client = new DefaultHttpClient();
+		HttpResponse response = client.execute(get);
+		result = EntityUtils.toString(response.getEntity(),"utf-8");
+        System.out.println(result);
+	}
+}
---
+++ src/main/java/com/course/chapter/httpClient/MyHttpClient.java
@@ -0,0 +1,31 @@
+package com.course.chapter.httpClient;
+
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class MyHttpClient {
+
+
+    @Test
+    public void test1() throws IOException {
+
+        //用来存放我们的结果
+        String result;
+        HttpGet get = new HttpGet("http://www.baidu.com");
+        //这个是用来执行get方法的
+        HttpClient client = new DefaultHttpClient();
+        HttpResponse response = client.execute(get);
+        //response.getEntity() 得到响应的全体内容
+        result = EntityUtils.toString(response.getEntity(),"utf-8");
+        
+        System.out.println(result);
+    }
+
+}
